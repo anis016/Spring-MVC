021da79573f0cd1570bd8d6b8cbe46888f29e83f
Sayed Anisul Hoque <anis.cuet016@gmail.com>
2020-05-20 08:59:57 +0200

    moved all the modules to a common module

--- docker/tomcat-docker.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# pull docker for JDK8
-docker pull tomcat:9.0.35-jdk8-openjdk
-docker run --detach --name tomcat -p 8888:8080 tomcat:9.0.35-jdk8-openjdk
-
-# https://docs.docker.com/engine/reference/run/#clean-up---rm
-# docker run -it --rm tomcat:9.0.35-jdk8-openjdk
--- spring-mvc-template/src/main/webapp/WEB-INF/view/main-menu.jsp
+++
@@ -1,7 +0,0 @@
-<!DOCTYPE html>
-<html>
-    <title>home page</title>
-<body>
-    <h2>Spring MVC Demo - Home Page</h2>
-<body>
-</html>
